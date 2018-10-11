39c130878784628b1021106d245038290b155364
GalinY <44042252+GalinY@users.noreply.github.com>
2018-10-11 02:49:55 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,35 +2,6 @@ package com.galin;
 
 public class Main {
 
-    public static void main(String[] args) {
-        int newScore = calculateScore ("Galin ", 500);
-        System.out.println("New score is " + newScore);
-        calculateScore(75);
-        calculateScore();
-        double centimeters = calcFeetAndInchesToCentimeters(6,13);
-        if (centimeters <0.0){
-            System.out.println("Invalid parameters");
-        }
-        calcFeetAndInchesToCentimeters(157);
-    }
-
-    public static int calculateScore (String playerName, int score) {
-        System.out.println("Player " + playerName + " scored " + score + " points");
-        return score * 1000;
-    }
-
-    public static int calculateScore(int score) {
-        System.out.println("Unnamed player " + " scored " + score + " points");
-        return score * 1000;
-    }
-
-    public static int calculateScore() {
-        System.out.println("No player name, no player score.");
-        return 0;
-    }
-
-
-
     public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
 
         if ((feet <0) || ((inches <0) || (inches >12))) {
